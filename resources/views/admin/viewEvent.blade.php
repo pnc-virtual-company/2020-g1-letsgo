18efa9bfc7a488f6d590b09526e3f5d7d50520b1
chanlylytouch <chanlylytouch8@gmail.com>
2020-07-12 10:04:54 +0700

    #18 correction event view

--- resources/views/admin/viewEvent.blade.php
+++ resources/views/admin/viewEvent.blade.php
@@ -6,34 +6,49 @@
   <br>  
   <div class="container">
       <div class="col-12">
-            <!-- Actual search box -->
-            <div class="form-group has-search">
-              <span class="fa fa-search form-control-feedback"></span>
-              <input type="text" class="form-control" placeholder="Search">
-            </div>
-            <h1>Events</h1>
-            <h5>Saturday,July 11</h5>
-        <div class="card mb-3" style="border-radius: 20px;">
-            <div class="card-body" >
-                {{-- foreach to show event --}}
-              <div class="row">
-                <div class="col-sm-3"><br><h2 class="text-secondary">8:00 PM</h2></div>
-                <div class="col-sm-4">
-                    <p><b>Bord game</b></p>
-                    <h2>The Duck is Back</h2>
-                    <p><strong class="text-warning">6</strong> member going</p>
-                </div>
-                <div class="col-sm-3">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSe0tC6P0G3n_CeYFdklK3aN5VEUjAJJV1Oag&usqp=CAU" style="width: 130px; height: 120px;"  class="img-thumbnail" alt="Cinque Terre">
-                </div>
-                <div class="col-sm-2">
-                    <br>
-                    <a href="#" style="font-size:22px" class="btn btn-outline-danger btn-center"><i class="fa fa-times-circle" style="color:red"></i>&nbsp;Remove</a>
-                </div>
+        <div class="form-group has-search mt-4">
+          <span class="fa fa-search form-control-feedback"></span>
+          <input type="text" class="form-control" placeholder="Search">
+      </div>
+      <h1 class="text-center"><b class="text-success">A</b>ll Events</h1><br>
+        <table class="table table-hover">
+          <thead>
+            <tr>
+              <th>Organizer</th>
+              <th>City</th>
+              <th>Title</th>
+              <th>Category</th>
+              <th>Start date</th>
+              
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td class="action">Ronan</td>
+              <td class="action">Vancouver</td>
+              <td class="action">Piano</td>
+              <td class="action">Music</td>
+              <td class="action">25/05/2020</td>
+              <td class="action_hidden">
+                <a href="" class="text-danger" data-toggle="modal" data-target="#removeCategory"><span class="material-icons text-danger">delete</span></a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <div class="modal" id="removeCategory">
+          <div class="modal-dialog">
+          <div class="modal-content">
+              <div class="modal-body">
+                  {{-- <form action="" method="POST"> --}}
+                      <h3 class="mb-4"><b>Remove Event</b></h3>
+                      <p>Are you sure you want to delete the Event?</p>
+                      <button type="button" class="btn btn-danger" data-dismiss="modal">DON'T REMOVE</button>
+                      <button type="submit" class="btn btn-warning float-right text-light ml-2">REMOVE</button>
+                  {{-- </form> --}}
               </div>
-              {{-- end foreach of event --}}
-            </div>
-        </div>
+          </div>
+          </div>
+      </div>
     </div>
 </div>
 
