e2f1d6ad525019f2f3190f68f06951f88abded1b
unknown <sreyta.sean04@gmail.com>
2020-07-15 14:38:47 +0700

    update style

--- resources/views/exploreEvent.blade.php
+++ resources/views/exploreEvent.blade.php
@@ -36,8 +36,8 @@
                 </div>
                 <div class="col-sm-4" data-toggle="modal" data-target="#eventDetail">
                     <p><b class="text-primary">Sport</b></p>
-                    <h4 class="text-warning">Running</h4>
-                    <p> <strong class="text-warning">6</strong> members going</p>
+                    <h4 class="text-success">Running</h4>
+                    <p> <strong class="text-success">6</strong> members going</p>
                 </div>
                 <div class="col-sm-3" data-toggle="modal" data-target="#eventDetail">
                     {{-- <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSe0tC6P0G3n_CeYFdklK3aN5VEUjAJJV1Oag&usqp=CAU" style="width: 130px; height: 120px;"  class="img-thumbnail" alt="Cinque Terre"> --}}
