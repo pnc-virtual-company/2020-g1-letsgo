b669db9695efc4bc9a11e4969f870b87df87fb17
Chorn Pothea <53595445+pothea88@users.noreply.github.com>
2020-07-22 14:10:27 +0700

    Update view.blade.php

--- resources/views/event/view.blade.php
+++ resources/views/event/view.blade.php
@@ -177,10 +177,10 @@
                 <label class="text-center">Picture</label>
                 <img class="mx-auto d-block" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSgoq4FpuZlsN3lQkqspHd5kYJsRUM8R30TMw&usqp=CAU" alt="..." width="105" style="border-radius: 105px;" height="105" alt="Avatar">
                 <div class="image-upload text-center">
-                  <label for="file-input">
+                  <label for="file-input2">
                     <i class="material-icons m-2 text-primary" style="cursor:pointer;">create</i>
                   </label>
-                  <input id="file-input" type="file" name="picture" hidden>
+                  <input id="file-input2" type="file" name="picture" hidden>
                 </div>
               </div>
 
