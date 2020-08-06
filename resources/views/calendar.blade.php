86a6148a38d753b0576fc5a46f6d0e187e80e0a4
chanlylytouch <chanlylytouch8@gmail.com>
2020-08-06 13:35:49 +0700

    checkbox only your event

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -61,8 +61,7 @@
       initialView: 'dayGridMonth',
       events: [
         @foreach($events as $event) {
-          title: '{{$event->title}}: <?php $date = new DateTime($event->start_time);
-                                      echo date_format($date, 'g:iA'); ?>',
+          title: '{{$event->title}}: <?php $date = new DateTime($event->start_time); echo date_format($date, 'g:iA'); ?>',
           start: '{{$event->start_date}}',
           end: '{{$event->end_date}}'
         },
